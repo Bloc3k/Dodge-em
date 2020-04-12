b746ac64097d39b57740a34f915c713f110a9d25
Jakub <calalamadolo@gmail.com>
2020-04-12 23:18:42 +0200

    Added sprites, reverse, bulets but they aren't oriented properly in 2 and 3 quadrant and speed of buttle is steel not set well.

--- ParkIt/Tank.js
+++ ParkIt/Tank.js
@@ -1,38 +1,88 @@
 class Tank {
-    constructor(x = 200,y = 300, speed = 4){
-        this.pos = createVector(200,300);
+    constructor( tank_sprite, bullet_sprite, x = 200, y = 300, speed = 4){
+        this.pos = createVector(x,y);
         this.heading = createVector(1,0);
         this.angle = 0;
         this.angSpeed = 2;
-        this.speed = 4;
-
+        this.speed = speed;
+        this.tank_sprite = tank_sprite;
+        this.bullet_sprite = bullet_sprite;
+        this.reloadTime = 200;
+        this.reloadCounter = 0;
+        this.loaded = true;
     }
 
     draw() {
         push();
-
+        
         //draw tank
-        fill(220,0,0);
         angleMode(DEGREES);
-        rectMode(CENTER);
-        translate( this.pos.x,this.pos.y)
+        translate( this.pos.x, this.pos.y)
         rotate(this.angle);
-        rect(0, 0, 50 , 50,6);
-            
-        pop();
+        image(this.tank_sprite,-40,-47);
+        
+        pop(); 
     }
 
     update() {
         if (keyIsDown(UP_ARROW)){
+            //go forward
             this.pos.add(this.heading);
         }
+        if (keyIsDown(DOWN_ARROW)){
+            //go backwards
+            this.pos.sub(this.heading);
+        }
         if (keyIsDown(LEFT_ARROW)){
+            //turn left
             this.angle -= this.angSpeed;
         }
         if (keyIsDown(RIGHT_ARROW)){
+            //turn right
             this.angle += this.angSpeed;
         }
-        this.heading.set(cos(this.angle) * this.speed, sin(this.angle) * this.speed);
+        if (keyIsDown(32)){
+            if (this.loaded){
+                //shoot bullet
+                bullets.push(new Bullet(this.pos, this.heading));
+                this.loaded = false;
+            }
+        }
+
+        //reloading
+        this.reloadCounter++;
+        if (this.reloadCounter >= this.reloadTime) {
+            this.loaded = true;
+            this.reloadCounter = 0;
+        }
+
+        //calculate new heading
+        this.heading.set(cos(this.angle - 90) * this.speed, sin(this.angle- 90) * this.speed);
+    }   
+
+}
+
+class Bullet {
+    constructor(pos, direction){
+        this.pos = createVector(pos.x, pos.y);
+        this.direction = createVector(direction.x / direction.mag(), direction.y / direction.mag());
+       // this.direction.div(direction.mag()).mult(this.speed);
+        this.speed = 10;
+        this.angle = atan( this.direction.y / this.direction.x) + 90;
+    }
+
+    draw(){
+        push();
+
+        translate(this.pos.x,this.pos.y);
+        rotate(this.angle);
+        image(bullet_sprite,-5,-10);
+
+        pop();
+    }
+
+    update(){
+        this.pos.add(this.direction);
     }
 
 }
--- ParkIt/sketch.js
+++ ParkIt/sketch.js
@@ -1,17 +1,31 @@
+let bullets = [];
+let tank;
+function preload() {
+  tank_sprite = loadImage('/ParkIt/Assets/tank_sprite.png');
+  bullet_sprite = loadImage('/ParkIt/Assets/bullet_sprite.png');
+}
+
 function setup() {
     createCanvas(innerWidth, innerHeight);
-    tank = new Tank();
+    tank = new Tank(tank_sprite, bullet_sprite);
   }
   
-  function draw() {
+function draw() {
     background(0);
 
     fill(230,230,230);
     textSize(60);
     text("Still in progress",innerWidth/2 - 150,innerHeight/2);
-    
-    
+  
+  
+    //draw bullets
+    for (let bullet of bullets){
+      
+        bullet.draw();
+        bullet.update();
+    }
+  
     tank.update();
     tank.draw();
 
-  }
+}

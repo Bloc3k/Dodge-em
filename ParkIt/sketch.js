c2ba5ff06cf9ec0ceb36ffff066843926285310d
Jakub <calalamadolo@gmail.com>
2020-04-13 23:14:50 +0200

    Added two tanks and movement as player.

--- ParkIt/sketch.js
+++ ParkIt/sketch.js
@@ -1,13 +1,19 @@
 let bullets = [];
 let tank;
+let enemies = [];
+
 function preload() {
   tank_sprite = loadImage('/ParkIt/Assets/tank_sprite.png');
-  bullet_sprite = loadImage('/ParkIt/Assets/bullet_sprite.png');
+  tankEnemy = loadImage('/ParkIt/Assets/enemyTank_sprite.png');
+  bullet_sprite = loadImage('/ParkIt/Assets/bulletPlayer_sprite.png');
 }
 
 function setup() {
     createCanvas(innerWidth, innerHeight);
     tank = new Tank(tank_sprite, bullet_sprite);
+    for (let i = 0; i < 2; i++) {
+      enemies.push(new Tank(tankEnemy, null, i * 300 + 100, 500));
+    }
   }
   
 function draw() {
@@ -19,10 +25,14 @@ function draw() {
   
   
     //draw bullets
-    for (let bullet of bullets){
-      
+    for (let bullet of bullets) {
         bullet.draw();
         bullet.update();
+    }
+
+    for (let enemy of enemies) {
+        enemy.draw();
+        enemy.update();
     }
   
     tank.update();

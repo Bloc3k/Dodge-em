57bef090aad20c1d07948c53a9c1dfac7e83bb52
Bloc3k <calalamadolo@gmail.com>
2021-06-22 22:45:19 +0200

    added local leaderboard

--- DodgeEm/Statistics.js
+++ DodgeEm/Statistics.js
@@ -4,8 +4,9 @@ class Leaderboard {
         this.height = 300;
         this.x = 20;
         this.y = innerHeight - this.height - 20;
-        this.topten = [["yakub", 14],["kaspis", 13], ["yakub", 14],["kaspis", 13], ["yakub", 14],["kaspis", 13], ["yakub", 14],["kaspis", 13], ["sefis", 11], ["sefis", 11]];
-        this.step = this.height/11;
+        this.topten = this.load();
+        this.step = this.height/10 - 1;
+        console.log(this.topten)
     }
 
     show() {
@@ -17,16 +18,102 @@ class Leaderboard {
         textSize(this.height/12);
         strokeWeight(1);
         fill(255);
-        for (let i = 0; i < this.topten.length || i < 9; i++) {
-            if (i == 9)
-                text(i+1,this.x + 2, this.y + (i+1) * this.step);
-            else 
-                text(i+1,this.x + 8, this.y + (i+1) * this.step);
-            text(this.topten[i][0], this.x + 40, this.y + (i+1) * this.step);
-            text(this.topten[i][1], this.x + 190, this.y + (i+1) * this.step);
+        for (let i = 0; i < 10; i++) {
+            if (i < this.topten.length) {                                    /*show name and score*/
+                if (i == 9)
+                    text(i+1,this.x + 2, this.y + (i+1) * this.step);
+                else 
+                    text(i+1,this.x + 8, this.y + (i+1) * this.step);
+                text(this.topten[i][0], this.x + 40, this.y + (i+1) * this.step);
+                text(this.topten[i][1], this.x + 190, this.y + (i+1) * this.step);
+            } else {
+                if (i == 9)                                                 /*show dashes when doesn't have record*/
+                    text(i+1,this.x + 2, this.y + (i+1) * this.step);
+                else 
+                    text(i+1,this.x + 8, this.y + (i+1) * this.step);
+                text("  -", this.x + 40, this.y + (i+1) * this.step);
+                text(" -", this.x + 190, this.y + (i+1) * this.step);
+            }
         }
         
-
         pop();
-    }   
+    } 
+
+    update(name, score) {
+        const leaderboard = this.topten;
+        
+        /*find where to put new record, put it there and shift rest*/
+        let i = 0, shift = false, previousRow;
+        for (; i < this.topten.length + 1; i++) {
+            if (i == this.topten.length) {
+                if (previousRow == null)    /*add record at the end*/
+                    this.topten.push([name, score]);
+                else                        /*shifting extends list*/
+                    this.topten.push(previousRow);
+                break;
+            } else {
+                if (shift == true) {                
+                    const temp = leaderboard[i];
+                    leaderboard[i] = previousRow;
+                    previousRow = temp;
+                }
+                if (score > leaderboard[i][1] && shift == false) {    
+                    previousRow = leaderboard[i]; 
+                    leaderboard[i] = [name, score];
+                    shift = true;   /*shift all upcoming rows*/
+                }
+            }
+        }
+
+        while (leaderboard.length > 10) {
+            leaderboard.pop();
+        }
+        
+        /*save updated leaderboard as cookie*/
+        this.save(leaderboard);
+    }
+   
+    load() {
+        const key = "leaderboard=";
+        const cookies = document.cookie.split(';');
+        let leaderboard_string = null;
+
+        for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        while (cookie.charAt(0) === ' ') {
+                cookie = cookie.substring(1, cookie.length);
+            }
+        if (cookie.indexOf(key) === 0) {
+                leaderboard_string = cookie.substring(key.length, cookie.length);
+            }
+        }
+        
+        const arrOfPlayersAndScores = leaderboard_string.split('_');      /*splits string to array of items "name-score"*/
+        const _leaderboard = [];
+        
+        for (let i = 0; i < arrOfPlayersAndScores.length; i++) {
+            _leaderboard.push(arrOfPlayersAndScores[i].split('-'));
+        }
+
+        /*refresh cookie expiration date*/
+        this.save(_leaderboard);
+
+        return _leaderboard;
+    }
+
+    save(leaderboard) {
+        const expiration = new Date();
+        expiration.setDate(expiration.getDate() + 1);  /*expires tomorrow*/
+        let leaderboard_string = "";
+        for (let record in leaderboard) {
+            leaderboard_string += "_" + leaderboard[record][0] + "-" + leaderboard[record][1]
+        }
+        leaderboard_string = leaderboard_string.substring(1, leaderboard_string.length)
+        document.cookie = "leaderboard=" + leaderboard_string + ";expires=" + expiration + ";";
+    }
+  
+    /*Remove cookies*/
+    clear() {
+      document.cookie = escape("leaderboard") + "=" + "NaN" + ";expires=" + new Date(-1).toUTCString() + ";";
+    }
 }
--- DodgeEm/sketch.js
+++ DodgeEm/sketch.js
@@ -3,7 +3,8 @@ var bullets = [];
 var lassers = [];
 var coin;
 var running = false;
-let leaderboard;
+var leaderboard;
+let scoring = false;
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
@@ -14,6 +15,7 @@ function setup() {
   for (let i = 0; i < innerWidth / 110; i++) {
     bullets[i] = new Bullet(i % 2);
   }
+  document.cookie = "leaderboard=Yakub-69_LuckyBoy-31_PEPEE-21_OMEGALUL-19_KEKW-16_toddler-15_newbie-11_noob-7_rookie-6_pleb-3";
 }
 
 function draw() {
@@ -37,8 +39,9 @@ function draw() {
     coin.draw();
     player.draw();
 
+    scoring = true;
   } else {
-    //----------------------- LOBBY ----------------------
+    //----------------------- LOBBY -----------------------
     for (let bullet of bullets) {
       bullet.draw();
     }
@@ -57,6 +60,17 @@ function draw() {
     text("Press any other key or mouse button to play again", innerWidth / 2 - 350, 80);
     text("Try to collect as many coins as possible without dying", innerWidth/2 - 300, innerHeight - 40);
 
+    /*add new record*/
+    if (coin.coinCount > leaderboard.topten[leaderboard.topten.length - 1][1]) {
+      if (scoring) { 
+        nickname = null;
+        while (nickname == null)
+        nickname = prompt("You're one of the best. You've earned a place in the Hell of Flame <3", "What's your nickname, sir?");
+        leaderboard.update(nickname, coin.coinCount);
+        scoring = false;
+      }
+    }
+
     /*Restart game*/
     if (keyIsPressed || mouseIsPressed) {
       running = true;
@@ -72,4 +86,7 @@ function draw() {
   coin.drawScore();
 
 }
-//TODO: Make leaderboard
+
+function newRecord() {
+  console.log(this.value);
+}

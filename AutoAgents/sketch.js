4ee5a6c662c85f379db648794da0d2854fda23a8
Bloc3k <calalamadolo@gmail.com>
2021-08-26 20:19:01 +0200

    Added sketch for Autonomous Characters showcase

---
+++ AutoAgents/sketch.js
@@ -0,0 +1,14 @@
+
+function setup() {
+    createCanvas(innerWidth,innerHeight);
+    
+}
+
+function draw() {
+    background(33);
+  
+
+    if (keyCode == 27)
+        window.history.back();
+  
+}

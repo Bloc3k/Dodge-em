454ca8c51c85490f0f504a7b7fc81421f938d40f
Bloc3k <calalamadolo@gmail.com>
2022-05-31 17:13:06 +0200

    Connection lost animation fixed

--- public/BattleArena/Animations/ConnectionLost.js
+++ public/BattleArena/Animations/ConnectionLost.js
@@ -12,9 +12,10 @@ class ConnectionLost extends Animation {
             fill(20, 20, 20, 200);
             noStroke();
             rectMode(CORNER)
-            rect(innerWidth/2-100, 13, 200, 40, 5);
+            rect(innerWidth/2-100, 25, 220, 40, 5);
             fill('#ccc');
             textSize(26);
+            textAlign(LEFT, CENTER)
             text('Connection lost', innerWidth/2-90, 40);
             pop();
         }
